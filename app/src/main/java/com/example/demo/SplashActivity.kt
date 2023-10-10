8afb1c229d9c25a09255ad48a1d428b466dc38fa
Harshil Patel <harshilrpatel7575@gmail.com>
2023-10-10 18:11:55 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/demo/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.demo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val demosplash : ImageView = findViewById(R.id.demoIcon)
+        demosplash.alpha = 0f
+        demosplash.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
